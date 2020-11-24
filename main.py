7bb44b6ce037820df9fd1748c7ff42a9567a74fe
savage-lof <lomova20042004@ya.ru>
2020-11-24 21:30:38 +0300

    final

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import sys
 import random
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
-from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
+import sqlite3
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect("coffee.db")
+        self.cur = self.con.cursor()
+        self.result = self.cur.execute("""SELECT * FROM Espresso""").fetchall()
+        self.select_data()
 
     def select_data(self):
-        # Получим результат запроса,
-        # который ввели в текстовое поле
-        query = self.textEdit.toPlainText()
-        res = self.connection.cursor().execute(query).fetchall()
-        # Заполним размеры таблицы
         self.tableWidget.setColumnCount(6)
         self.tableWidget.setRowCount(0)
-        # Заполняем таблицу элементами
-        for i, row in enumerate(res):
+        for i, row in enumerate(self.result):
             self.tableWidget.setRowCount(
                     self.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
